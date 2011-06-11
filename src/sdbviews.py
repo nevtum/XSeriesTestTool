1925a0a25c7d825ae8d2a02b3b5b28f47ec1ae4d
Neville Tummon <nt.devs@gmail.com>
2011-06-11 04:50:38 -0700

    Views now take use of added sdb functionality

--- src/sdbviews.py
+++ src/sdbviews.py
@@ -10,12 +10,12 @@ class sdbreporter:
         assert (isinstance(sdbmodel, sdbMdl))
         self.sdbmdl = sdbmodel
         self.sdbmdl.register(self)
-    
+
     def printmachineinfo(self, packet):
         print "Version #:               %s" % packet.versionNr()
         print "Machine ID:              %s" % packet.GMID()
         print "Machine Idle:            %s" % packet.idle()
-        
+
     def printmeters(self, packet):
         print "Turnover:                %s" % packet.turnover()
         print "Total Wins:              %s" % packet.totalWins()
@@ -32,7 +32,7 @@ class sdbreporter:
         print "Games Since Reboot:      %s" % packet.gamesSinceReboot()
         print "Games Since Door Open:   %s" % packet.gamesSinceDoorOpen()
         print "Base Credit Value:       %s" % packet.baseCreditValue()
-        
+
     def printstatusbytes(self, packet):
         print "Game Cycle:              %s" % packet.gameCycle()
         print "Powered Up:              %s" % packet.powerUp()
@@ -59,23 +59,32 @@ class sdbreporter:
         print "Manufacturer Error1:     %s" % packet.manufacturerError0()
         print "Manufacturer Error2:     %s" % packet.manufacturerError1()
         print "Cancel Credit Error:     %s" % packet.cancelCreditError()
-        
+
+    def printSEFPorts(self, packet):
+        print "SEF Port 1               %s" % packet.port1Status()
+        print "SEF Port 2               %s" % packet.port2Status()
+        print "SEF Port 3               %s" % packet.port3Status()
+        print "SEF Port 4               %s" % packet.port4Status()
+        print "SEF Port 5               %s" % packet.port5Status()
+        print "SEF Port 6               %s" % packet.port6Status()
+
     def printprogramIDs(self, packet):
         print "Program ID 1:            %s" % packet.programID1()
         print "Program ID 2:            %s" % packet.programID2()
         print "Program ID 3:            %s" % packet.programID3()
         print "Program ID 4:            %s" % packet.programID4()
-    
+
     def printRTP(self, packet):
         print "PRTP:                    %s" % packet.prtp()
-        
+
     def printLinkProgSupport(self, packet):
         print "Link Progr. Supported:   %s" % packet.linkedProgSupported()
-    
+
     def update(self):
         self.printmachineinfo(self.sdbmdl)
         self.printstatusbytes(self.sdbmdl)
         self.printmeters(self.sdbmdl)
+        self.printSEFPorts(self.sdbmdl)
         self.printprogramIDs(self.sdbmdl)
         self.printRTP(self.sdbmdl)
         self.printLinkProgSupport(self.sdbmdl)
