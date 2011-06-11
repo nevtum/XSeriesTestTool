46a47cb1679f60e530220caa36fcc6d686c53c6e
Neville Tummon <nt.devs@gmail.com>
2011-06-10 22:25:35 -0700

    First Submission

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from datablockmodels import *
+from sdbviews import *
+from generators import *
+
+file = open('SDB.MDB.Raw.Data.txt', 'r')
+a = charfilter(file, '.', ' ', '\n', '\r', '\t')
+b = charpacket(a, size = 2)
+c = datablockdispatcher(b)
+d = datablockfilter(c, '00', '22')
+e = diffpacketfilter(d)
+
+sdbhandle = sdbMdl() # Model
+view = sdbreporter(sdbhandle) # View
+
+packetswitch = packetswitch()
+packetswitch.registermodelinstance('00', sdbhandle)
+#packetswitch.registermodelinstance('22', mdbMdl())
+packetswitch.setstream(e)
+packetswitch.run()
