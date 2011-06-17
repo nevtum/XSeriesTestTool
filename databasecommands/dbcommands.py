2fdd885a5cb44821bf7eb0201bdd4844a3343d77
Neville Tummon <nt.devs@gmail.com>
2011-06-17 22:33:17 +1000

    Adding initial functionality of command

--- databasecommands/dbcommands.py
+++ databasecommands/dbcommands.py
@@ -4,14 +4,26 @@ Created on 17/06/2011
 @author: nEVSTER
 '''
 
-class MyClass(object):
+class insertToDBCommand(object):
     '''
     classdocs
     '''
 
 
-    def __init__(selfparams):
-        '''
-        Constructor
-        '''
+    def __init__(self, data):
+        self.data = data
+        self.array = self.fillarray()
+        
+    def fillarray(self):
+        hexdata = []
+        hexdata.append(self.convert(self.data[2:3]))
+        return hexdata
+    
+    def convert(self, data):
+        x = ''.join(data)
+        return int(x, 16)
+    
+    def item(self, n):
+        return self.array[n]
+
         
--- databasecommands/unittest_dbcommands.py
+++ databasecommands/unittest_dbcommands.py
@@ -4,19 +4,22 @@ Created on 17/06/2011
 @author: nEVSTER
 '''
 import unittest
+from dbcommands import *
 
 
 class Test(unittest.TestCase):
     def setUp(self):
         f = open('sdbmockdata.txt', 'r')
-        data1 = f.readline().split()
+        self.data = f.readline().split()
 
     def tearDown(self):
         pass
     
     def testName(self):
+        cmd = insertToDBCommand(self.data)
         self.assertEquals(0x1234, cmd.convert(['12', '34']))
-        self.assertEquals(0x)
+        self.assertEquals(0xabcd12, cmd.convert(['ab', 'cd', '12']))
+        self.assertEquals(0x00, cmd.item(0))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
