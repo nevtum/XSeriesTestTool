4bc6cb5b8946d13122d014a3d6edb22b577b4dbc
Neville Tummon <nt.devs@gmail.com>
2011-06-24 23:05:59 +1000

    simple solution loopback server/client created

--- src/tcpclient.py
+++ src/tcpclient.py
@@ -4,4 +4,4 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("localhost", 5000))
 while 1:
     data = client_socket.recv(128)
-    print "RECIEVED:" , data
+    print data, len(data)
--- src/tcpserver.py
+++ src/tcpserver.py
@@ -6,12 +6,16 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("", 5000))
 server_socket.listen(5)
 
+file = open('SDB.MDB.Raw.Data.txt', 'r')
+a = charfilter(file, '.', ' ', '\n', '\r', '\t')
+b = charpacket(a, size = 2)
+c = datablockdispatcher(b)
+
 print "TCPServer Waiting for client on port 5000"
 
 while 1:
 	client_socket, address = server_socket.accept()
 	print "Connection established with: ", address
-	while 1:
-		data = '1'
-		client_socket.send(data)
+	for eachpacket in c:
+		client_socket.send(''.join(eachpacket))
 		time.sleep(0.5)
