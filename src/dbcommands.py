69c22d49c285cd79943dbda969f8518eb61ff5ef
Neville Tummon <nt.devs@gmail.com>
2011-07-02 17:06:19 +1000

    completing metadata schema with database commands

--- src/dbcommands.py
+++ src/dbcommands.py
@@ -25,40 +25,11 @@ class createSDBTableCommand:
         except sqlite3.OperationalError:
             print "error creating tables"
     def createtable(self):
-        metadata = (('EventID', 'INTEGER PRIMARY KEY'),
-                    ('ID', 'TEXT'), # try parsing from XML file instead
-                    ('VersionNr', 'TEXT'),
-                    ('GMID', 'TEXT'),
-                    ('StatusByte1', 'TEXT'),
-                    ('StatusByte2', 'TEXT'),
-                    ('StatusByte3', 'TEXT'),
-                    ('StatusByte4', 'TEXT'),
-                    ('StatusByte5', 'TEXT'),
-                    ('MultiGameNumber', 'TEXT'),
-                    ('MultiGameCombNumber', 'TEXT'),
-                    ('Turnover', 'TEXT'),
-                    ('TotalWins', 'TEXT'),
-                    ('CashBox', 'TEXT'),
-                    ('CancelledCredits', 'TEXT'),
-                    ('GamesPlayed', 'TEXT'),
-                    ('MoneyIn', 'TEXT'),
-                    ('MoneyOut', 'TEXT'),
-                    ('CashIn', 'TEXT'),
-                    ('CashOut', 'TEXT'),
-                    ('CurrentCredits', 'TEXT'),
-                    ('MiscAccrual', 'TEXT'),
-                    ('NrPowerUps', 'TEXT'),
-                    ('GamesSinceLastPowerUp', 'TEXT'),
-                    ('GamesSinceLastDoorOpen', 'TEXT'),
-                    ('PortStatusByte', 'TEXT'),
-                    ('BaseCreditValue', 'TEXT'),
-                    ('programID1', 'TEXT'),
-                    ('programID2', 'TEXT'),
-                    ('programID3', 'TEXT'),
-                    ('programID4', 'TEXT'),
-                    ('PRTP', 'TEXT'),
-                    ('SecondaryFunctions', 'TEXT'))
-        string = ['%s %s' % kvpair for kvpair in metadata]
+        meta  = [('EventID', 'INTEGER PRIMARY KEY')]
+        for item in self.meta_elems:
+            meta.append((item.encode(), 'TEXT'))
+        string = ['%s %s' % kvpair for kvpair in meta]
+        print string
         table_info = ', '.join(string)
         cursor = self.database.cursor()
         sql = """CREATE TABLE Packets(%s)""" % table_info
@@ -78,19 +49,18 @@ class insertToDBCommand(object):
         self.data = data
         self.fillarray()
         self.database = database
+        xml = minidom.parse('packets.xml')
+        self.meta_elems = []
+        for ch in xml.firstChild.childNodes:
+            if ch.nodeType == 1:
+                #t = ch.firstChild.nextSibling
+                #t.firstChild.data
+                self.meta_elems.append(ch.localName)
         
     def execute(self):
-        metadata = ['ID', 'VersionNr', 'GMID', 'StatusByte1',
-        'StatusByte2', 'StatusByte3','StatusByte4', 'StatusByte5',
-        'MultiGameNumber', 'MultiGameCombNumber', 'Turnover',
-        'TotalWins', 'CashBox', 'CancelledCredits', 'GamesPlayed',
-        'MoneyIn', 'MoneyOut', 'CashIn', 'CashOut', 'CurrentCredits',
-        'MiscAccrual', 'NrPowerUps', 'GamesSinceLastPowerUp',
-        'GamesSinceLastDoorOpen', 'PortStatusByte',
-        'BaseCreditValue', 'programID1', 'programID2',
-        'programID3', 'programID4', 'PRTP', 'SecondaryFunctions']
+        meta = self.meta_elems
         cursor = self.database.cursor()
-        columns = ','.join(metadata)
+        columns = ','.join(meta)
         sql = '''INSERT INTO Packets(%s) VALUES(%s)''' % (columns, str(self.array).strip('[]'))
         cursor.execute(sql)
         vec = (strftime('"%Y-%m-%d %H:%M:%S"'), '"EGM"')
