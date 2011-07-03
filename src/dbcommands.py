e643d57396848737dea952780a0529fcf6efc00c
Neville Tummon <nt.devs@gmail.com>
2011-07-03 13:48:53 +1000

    completed db command meta methods

--- src/dbcommands.py
+++ src/dbcommands.py
@@ -7,20 +7,31 @@ import sqlite3
 from time import strftime
 from xml.dom import minidom
 
+def getmetaitems():
+    xml = minidom.parse('packets.xml')
+    meta_elems = []
+    for ch in xml.firstChild.childNodes:
+        if ch.nodeType == 1:
+            #t = ch.firstChild.nextSibling
+            #t.firstChild.data
+            meta_elems.append(ch.localName)
+    return meta_elems
+
+def getmetaranges():
+    xml = minidom.parse('packets.xml')
+    meta_ranges = []
+    for ch in xml.firstChild.childNodes:
+        if ch.nodeType == 1:
+            x = ch.attributes
+            try:
+                meta_ranges.append((int(x['startbyte'].value), int(x['endbyte'].value)))
+            except KeyError:
+                meta_ranges.append((int(x['byte'].value), int(x['byte'].value)))
+    return meta_ranges
+
 class createSDBTableCommand:
     def __init__(self, database):
         self.database = database
-        self.meta_elems = self.getmetaitems('packets.xml')
-
-    def getmetaitems(self, filename):
-        xml = minidom.parse(filename)
-        meta_elems = []
-        for ch in xml.firstChild.childNodes:
-            if ch.nodeType == 1:
-                #t = ch.firstChild.nextSibling
-                #t.firstChild.data
-                meta_elems.append(ch.localName)
-        return meta_elems
 
     def execute(self):
         try:
@@ -30,8 +41,8 @@ class createSDBTableCommand:
             print "error creating tables"
 
     def createtable(self):
-        meta  = [('EventID', 'INTEGER PRIMARY KEY')]
-        for item in self.meta_elems:
+        meta = [('EventID', 'INTEGER PRIMARY KEY')]
+        for item in getmetaitems():
             meta.append((item.encode(), 'TEXT'))
         string = ['%s %s' % kvpair for kvpair in meta]
         table_info = ', '.join(string)
@@ -53,21 +64,10 @@ class insertToDBCommand(object):
         self.data = data
         self.fillarray()
         self.database = database
-        self.meta_elems = self.getmetaitems('packets.xml')
-        
-    def getmetaitems(self, filename):
-        xml = minidom.parse(filename)
-        meta_elems = []
-        for ch in xml.firstChild.childNodes:
-            if ch.nodeType == 1:
-                #t = ch.firstChild.nextSibling
-                #t.firstChild.data
-                meta_elems.append(ch.localName)
-        return meta_elems
         
     def execute(self):
         cursor = self.database.cursor()
-        columns = ','.join(self.meta_elems)
+        columns = ','.join(getmetaitems())
         sql = '''INSERT INTO Packets(%s) VALUES(%s)''' % (columns, str(self.array).strip('[]'))
         cursor.execute(sql)
         vec = (strftime('"%Y-%m-%d %H:%M:%S"'), '"EGM"')
@@ -75,19 +75,11 @@ class insertToDBCommand(object):
         cursor.execute(sql)
         
     def getByteVector(self, lbound, hbound):
-        return self.data[lbound-1:hbound]
+        return self.data[lbound - 1:hbound]
         
     def fillarray(self):
-        ranges = [(2,2), (3,4), (6,8), (9,9), (10,10), (11,11), # utilise metadata instead
-                       (12,12), (13,13), (15,15), (16,16), (17,21),
-                       (22,26), (27,31), (32,36), (37,40), (42,46),
-                       (47,51), (52,56), (57,61), (62,66), (67,71),
-                       (72,75), (76,79), (80,83), (84,84), (85,86),
-                       (88,95), (96,103), (104,111), (112,119),
-                       (120,121), (122,122)]
-        
         hexdata = []
-        for lbound, hbound in ranges:
+        for lbound, hbound in getmetaranges():
             hexdata.append(self.convert(self.getByteVector(lbound, hbound)))
             
         self.array = hexdata # needed for SQL Query
@@ -135,3 +127,4 @@ database.commit()
 database.close()
 
         
+
