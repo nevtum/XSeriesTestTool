dfadc413b65d7d24c21773ee079fb17bf1b5fb84
Neville Tummon <nt.devs@gmail.com>
2011-07-07 00:11:47 +1000

    working simple version forwarder receiver

--- src/tcpclient.py
+++ src/tcpclient.py
@@ -1,7 +1,21 @@
-# TCP client example
 import socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("localhost", 5000))
-while 1:
-    data = client_socket.recv(128)
-    print data, len(data)
+
+class forwarder:
+    # public interface
+    def transmitMsg(self): # commscontroller calls this method
+        ''' encode(message)
+        ... transmit()'''
+    def setreceiver(self, receiver):
+        self.receiver = receiver
+    # private interface specific to forwarder/receiver
+    def transmit(self): # specific to receiver only
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect(("localhost", 5000))
+        while 1:
+            client_socket.send('Hey!!')
+            client_socket.close()
+    def encode(self, message):
+        pass
+    
+f = forwarder()
+f.transmit()
--- src/tcpserver.py
+++ src/tcpserver.py
@@ -1,44 +1,11 @@
-# TCP server example
-from generators import *
 import time
 import socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("", 5000))
-server_socket.listen(5)
-
-file = open('SDB.MDB.Raw.Data.txt', 'r')
-a = charfilter(file, '.', ' ', '\n', '\r', '\t')
-b = charpacket(a, size = 2)
-c = datablockdispatcher(b)
-
-print "TCPServer Waiting for client on port 5000"
-
-while 1:
-	client_socket, address = server_socket.accept()
-	print "Connection established with: ", address
-	for eachpacket in c:
-		client_socket.send(''.join(eachpacket))
-		time.sleep(0.5)
-
-class forwarder:
-	# public interface
-	def transmitMsg(self): # commscontroller calls this method
-		''' encode(message)
-		... transmit()'''
-	def setreceiver(self, receiver):
-		self.receiver = receiver
-	# private interface specific to forwarder/receiver
-	def transmit(self): # specific to receiver only
-		''' set up transmission to dispatch
-		... packet to receiver.
-		... when finished close connection'''
-	def encode(self, message):
-		pass
 
 class receiver:
 	# public interface
-	def recieveMsg(self): # commscontroller calls this method
+	def receiveMsg(self): # commscontroller calls this method
 		''' return decode(receive())'''
+		self.receive()
 	def setforwarder(self, forwarder):
 		self.forwarder = forwarder
 	# private interface specific to forwarder/receiver
@@ -47,6 +14,16 @@ class receiver:
 		... packet from forwarder. Optional timeout.
 		... process message and close connection.
 		... return message.'''
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.bind(("", 5000))
+		server_socket.listen(1)
+		while 1:
+			print 'receiver waiting...'
+			conn, address = server_socket.accept()
+			print "Connection established with: ", address
+			data = conn.recv(20)
+			print 'message: %s' % data
+
 	def decode(self, message):
 		pass
 
@@ -54,3 +31,6 @@ class commscontroller:
 	def __init__(self, forwarder, reciever):
 		self.forwarder = forwarder
 		self.reciever = reciever
+		
+x = receiver()
+x.receive()
