27afa72183083f5bcbffbd1eb33efe01c8735067
Neville Tummon <nt.devs@gmail.com>
2011-07-17 23:10:24 +1000

    attempting asyncronous forwarder-receiver

--- src/receiver.py
+++ src/receiver.py
@@ -1,6 +1,52 @@
 import time
 import socket
 
+####
+import asyncore
+
+class asyncreceiver(asyncore.dispatcher):
+	def __init__(self, hostname, portnr):
+		asyncore.dispatcher.__init__(self)
+		self.hostname = hostname
+		self.portnr = portnr
+		self.msg = None
+		self.startservice()
+	def startservice(self):
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind((self.hostname, self.portnr))
+		self.listen(1)
+	def handle_accept(self):
+		self.accept()
+		print 'accepted connection from client...'
+	def handle_read(self):
+		self.msg = self.recv(256)
+		print 'received: %s' % self.msg
+	def handle_close(self):
+		self.close()
+		self.startservice()
+	def receiveMsg(self):
+		return self.msg
+
+class asyncforwarder(asyncore.dispatcher):
+	def __init__(self, hostname, portnr):
+		asyncore.dispatcher.__init__(self)
+		self.hostname = hostname
+		self.portnr = portnr
+	def transmitMsg(self, msg):
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connect((self.hostname, self.portnr))
+		self.send(msg)
+		self.close()
+
+f = asyncforwarder('localhost', 2020)
+r = asyncreceiver('', 2020)
+f.transmitMsg('hey there maddie!')
+asyncore.loop()
+f.transmitMsg('hey there maddie!')
+
+####
+
 class receiver:
 	# public interface
 	def __init__(self, hostname, portnr):
