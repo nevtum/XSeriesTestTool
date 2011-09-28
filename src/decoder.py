12adb634fc026207991d64fcbdf62316af5ce0a6
Neville Tummon <nt.devs@gmail.com>
2011-09-27 21:42:01 -0700

    defining metadata from NSW mdb specification

--- src/decoder.py
+++ src/decoder.py
@@ -119,13 +119,14 @@ file = open('unittests/SDB.MDB.Raw.Data.txt', 'r')
 a = charfilter(file, '.', ' ', '\n', '\r', '\t') # filter out given characters
 b = charpacket(a, size = 2) # number of characters to extract from stream
 c = datablockdispatcher(b) # extract only standard XSeries Packets
-d = datablockfilter(c, '00', '22') # select packets that match packet IDs
+d = datablockfilter(c, '22') # select packets that match packet IDs
    
 xdec = XProtocolDecoder()
 xdec.registerTypeDecoder('integer-reverse', reverseIntegerDecoder)
 xdec.registerTypeDecoder('currency-reverse', reverseCurrencyDecoder)
 xdec.registerTypeDecoder('boolean', booleanDecoder)
 xdec.registerTypeDecoder('ascii-reverse', reverseAsciiDecoder)
+
 
 for packet in d:
     xdec.createXMLPacket(packet)
