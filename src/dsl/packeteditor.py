d0580ff2fd7def41d6b1c3a7e1bef677f200b4f9
Neville Tummon <nt.devs@gmail.com>
2011-11-15 22:41:52 +1100

    Added functions to put Hex/Ascii into an array of hexes

--- src/dsl/packeteditor.py
+++ src/dsl/packeteditor.py
@@ -6,5 +6,14 @@ Created on Nov 14, 2011
 
 class PacketEditor(object):
     def makeEmptyPacket(self, size):
-        return ['00']*size
+        return [hex(0)]*size
         
+    def putHex(self, packet, position, iVal):
+        if not (0x0 <= iVal <= 0xff):
+            raise ValueError
+        packet[position] = hex(iVal)
+        
+    def putAscii(self, packet, string, startpos, endpos):
+        x = [hex(ord(i)) for i in list(string)]
+        for index in xrange(startpos, endpos+1):
+            packet[startpos+index] = x[index]
--- src/unittests/unittest_packeteditor.py
+++ src/unittests/unittest_packeteditor.py
@@ -17,11 +17,37 @@ class Test(unittest.TestCase):
         self.assertEquals(5, len(packet1))
         self.assertEquals(3, len(packet2))
         for elem in packet2:
-            self.assertEquals('00', elem)
+            self.assertEquals('0x0', elem)
     
-    def testPutX(self):
+    def testClone(self):
         pass
     
+    def testPutHex(self):
+        packet = self.pe.makeEmptyPacket(4)
+        self.assertRaises(ValueError, self.pe.putHex, packet, 4, 0xff1234)
+        self.assertRaises(ValueError, self.pe.putHex, packet, 3, -12)
+        self.assertRaises(ValueError, self.pe.putHex, packet, 3, 'not valid')
+        self.assertRaises(IndexError, self.pe.putHex, packet, 5, 0x01)
+        self.pe.putHex(packet, 0, 0x30)
+        self.pe.putHex(packet, 1, 0xab)
+        self.assertEquals(packet[0], '0x30')
+        self.assertEquals(packet[1], '0xab')
+        
+    def testPutAscii(self):
+        test1 = self.pe.makeEmptyPacket(5)
+        self.pe.putAscii(test1, 'test', 0, 3)
+        result = ['0x74', '0x65', '0x73', '0x74', '0x0']
+        self.assertEquals(result, test1)
+        
+        test2 = self.pe.makeEmptyPacket(5)
+        self.pe.putAscii(test2, 'test', 0, 4)
+        self.assertEquals(result, test2)
+        
+        test3 = self.pe.makeEmptyPacket(5)
+        self.pe.putAscii(test3, 'test', 3, 4)
+        result = ['0x0', '0x0', '0x0', '0x74', '0x65']
+        self.assertEquals(result, test3)
+        
     def testBitToggle(self):
         pass
     
