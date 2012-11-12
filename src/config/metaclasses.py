433a69c7e2d5438fab58965b51fc7d5dfa73b6db
Neville Tummon <nt.devs@gmail.com>
2012-11-12 21:32:51 +1100

    applying GPL v3 licence

--- src/config/metaclasses.py
+++ src/config/metaclasses.py
@@ -1,8 +1,20 @@
-'''
-Created on 31/08/2011
+"""
+XSeriesTestTool - A NSW gaming protocol decoder/analyzer
+    Copyright (C) 2012  Neville Tummon
 
-@author: ntummon
-'''
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 from xml.etree import ElementTree as cElementTree
 
