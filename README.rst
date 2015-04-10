a29e9d924cde2470cc4fd3dbfef0b7a8f47d260e
Venkateshpitta@gmail.com <Venkatesh Pitta>
2015-04-10 20:04:47 +1000

    Added a readme and some more details to setup.py

---
+++ README.rst
@@ -0,0 +1,7 @@
+XSeriesTestTool
+---------------
+
+To run, enter XSeriesTestTool/XSeriesTestTool/ and do 'python app.py'
+
+This tool helps in analysing and studying data blocks emitted by
+gaming machines that follow NSW XSeries protocol.
--- setup.py
+++ setup.py
@@ -18,12 +18,29 @@ XSeriesTestTool - A NSW gaming protocol decoder/analyzer
 
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name = 'XSeriesTestTool',
       version = '0.1',
       description = 'A tool to help study and analyse NSW XSeries protocol compliant data blocks.',
+      long_description = readme(),
+      classifiers = [
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: GPLv3',
+          'Development Status :: under constant development through feedback',
+      ],
+      keywords = 'gambling Australia New South Wales NSW X XSeries XComm',
       url = 'https://github.com/nevtum/XSeriesTestTool',
       author = 'Neville Tummon',
       #author_email = 'nevilletummon@email.com',
-      licence = 'GPLv3',
+      license = 'GPLv3',
       packages = ['XSeriesTestTool'],
+      install_requires = [
+          'PyQt4',
+          'sqlite3',
+          'pyserial',
+      ],
+      include_package_data = True,
       zip_safe = False)
