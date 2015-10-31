c2f990d0fd593d381ed761ea1ee9ee118f4d58ca
Neville Tummon <nt.devs@gmail.com>
2015-10-31 11:01:50 +1100

    gpl v3 license placed in root dir instead

--- setup.py
+++ setup.py
@@ -1,21 +1,3 @@
-"""
-XSeriesTestTool - A NSW gaming protocol decoder/analyzer
-    Copyright (C) 2012  Neville Tummon
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
 from setuptools import setup
 
 def readme():
@@ -27,7 +9,6 @@ setup(name = 'XSeriesTestTool',
       description = 'A tool to help study and analyse NSW XSeries protocol compliant data blocks.',
       long_description = readme(),
       classifiers = [
-          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.4',
           'License :: OSI Approved :: GPLv3',
           'Development Status :: under constant development through feedback',
