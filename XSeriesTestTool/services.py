9dda32370a0f829eb1072c7c45c82403a5abdb40
Neville Tummon <nt.devs@gmail.com>
2015-11-01 10:51:34 +1100

    remove redundant methods from queryengine

--- XSeriesTestTool/services.py
+++ XSeriesTestTool/services.py
@@ -8,21 +8,20 @@ class QueryEngine(QObject):
     def __init__(self, filename, parent = None):
         QObject.__init__(self, parent)
         self.context = self._create_context(filename)
-        self.create_sql_tables()
+        self._create_sql_tables()
     
     def insert(self, direction, packet_type, byte_array):
         loggedtime = str(datetime.now())
         self._insert_changed_packet(direction, packet_type, byte_array, loggedtime)   
         self._insert_received_packet(packet_type, byte_array, loggedtime)
     
-    def get_last_packet(self, packet_type):
-        sql = """SELECT MAX(ID)
-        FROM distinctpackets
-        WHERE Class = '%s'
-        AND Direction = 'incoming'""" % packet_type
-        return self._get_records(sql)
+    def clear_database(self):
+        query = QtSql.QSqlQuery(self.context)
+        query.exec_("DELETE FROM session")
+        query.exec_("DELETE FROM distinctpackets")
+        query.finish()
     
-    def get_latest_packet(self, packet_type):
+    def _get_latest_packet(self, packet_type):
         sql = """SELECT *
         FROM distinctpackets
         WHERE Class = '%s'
@@ -38,7 +37,7 @@ class QueryEngine(QObject):
             return row_id, timestamp, data
         return None, None, None
     
-    def create_sql_tables(self):
+    def _create_sql_tables(self):
         query = QtSql.QSqlQuery(self.context)
         sql = """CREATE TABLE IF NOT EXISTS session(
         Timestamp DATETIME,
@@ -55,12 +54,6 @@ class QueryEngine(QObject):
         query.exec_()
         query.finish()
     
-    def clear_database(self):
-        query = QtSql.QSqlQuery(self.context)
-        query.exec_("DELETE FROM session")
-        query.exec_("DELETE FROM distinctpackets")
-        query.finish()
-    
     def _create_context(self, filename):
         context = QtSql.QSqlDatabase.addDatabase("QSQLITE")
         context.setDatabaseName(filename)
@@ -68,7 +61,7 @@ class QueryEngine(QObject):
         return context
         
     def _has_changed(self, packet_type, byte_array):
-        row_id, timestamp, data = self.get_latest_packet(packet_type)
+        row_id, timestamp, data = self._get_latest_packet(packet_type)
         if row_id is None:
             return True
 
@@ -91,31 +84,13 @@ class QueryEngine(QObject):
         query.finish()
         
     def _insert_received_packet(self, packet_type, byte_array, logged_time):
-        row_id = self._get_row_id_of_latest_packet(packet_type)
+        row_id, timestamp, data = self._get_latest_packet(packet_type)
         query = QtSql.QSqlQuery(self.context)
         query.prepare("INSERT INTO session(Timestamp, PacketID) VALUES(:date,:packetid)")
         query.bindValue(":date", logged_time)
         query.bindValue(":packetid", row_id)
         query.exec_()
         query.finish()
-                
-    def _get_row_id_of_latest_packet(self, packet_type):
-        return self.get_last_packet(packet_type)[0]
-    
-    def _get_records(self, sql):
-        query = QtSql.QSqlQuery(self.context)
-        if query.isActive():
-            debug.Log("Wrapper: previous query is still active")
-            return []
-        query.prepare(sql)
-        if query.exec_():
-            list = []
-            while query.next():
-                list.append(str(query.value(0)))
-            debug.Log("Wrapper: %i" % len(list))
-            return list
-        debug.Log("Wrapper: query did not execute successfully")
-        return []
     
     def __del__(self):
         self.context.close()
