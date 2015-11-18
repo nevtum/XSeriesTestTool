3bfefe84667b86358fb792d30d0f70971effc16d
Neville Tummon <nt.devs@gmail.com>
2015-11-18 20:57:50 +1100

    created xpacket as a wrapper around record

--- XSeriesTestTool/gui/decoder_view.py
+++ XSeriesTestTool/gui/decoder_view.py
@@ -1,5 +1,6 @@
 from PyQt4 import uic
 from PyQt4.QtCore import SIGNAL
+from xpacket import XPacket
 
 decbase, decform = uic.loadUiType("gui/packetview.ui")
 
@@ -22,13 +23,6 @@ class DecoderDialog(decbase, decform):
         self.textEdit.selectAll()
         self.textEdit.copy()
 
-    def _to_pretty_print(self, seq):
-        packetname, array = self.decoder.getDecodedData(seq)
-        mystring = "Packet: %s\n" % packetname
-        for key, value in array:
-            mystring += "    {0:50s}\t{1}\n".format(key, value)
-        return mystring
-
     def Update(self, newMdlIndex, oldMdlIndex):
         self._update_current_packet_view(newMdlIndex)
         self._update_old_packet_view(oldMdlIndex)
@@ -37,53 +31,32 @@ class DecoderDialog(decbase, decform):
     def _update_current_packet_view(self, newMdlIndex):
         if newMdlIndex.isValid():
             record = self._get_record(newMdlIndex)
-            sequence = self._to_sequence(record)
-            timestamp = record.value("LastChanged")
-            raw_data = self._format_raw_data(record)
-            self.uiSelected.setText(self._to_pretty_print(sequence))
-            self.lineEdit.setText(timestamp)
-            self.uiRawData.setText(raw_data)
+            packet = self._to_packet(record)
+            self.uiSelected.setText(packet.pretty_print(self.decoder))
+            self.lineEdit.setText(packet.get_time())
+            self.uiRawData.setText(packet.format_raw_data())
             
     def _update_old_packet_view(self, oldMdlIndex):
         if oldMdlIndex.isValid():
             record = self._get_record(oldMdlIndex)
-            sequence = self._to_sequence(record)
-            self.uiDeselected.setText(self._to_pretty_print(sequence))
+            packet = self._to_packet(record)
+            self.uiDeselected.setText(packet.pretty_print(self.decoder))
     
     def _update_differences_view(self, newMdlIndex, oldMdlIndex):
         if newMdlIndex.isValid() & oldMdlIndex.isValid():
-            newseq = self._to_sequence(self._get_record(newMdlIndex))
-            oldseq = self._to_sequence(self._get_record(oldMdlIndex))
-            self.uiChangeSet.setText(self._format_before_after(newseq, oldseq))
+            new_packet = self._to_packet(self._get_record(newMdlIndex))
+            old_packet = self._to_packet(self._get_record(oldMdlIndex))
+            changes = new_packet.changes_since(old_packet, self.decoder)
+            self.uiChangeSet.setText(changes)
         
     def _get_record(self, model_index):
         proxy = self.dvm.getProxyModel()
         true_index = proxy.mapToSource(model_index)
         origmdl = proxy.sourceModel()
         return origmdl.record(true_index.row())
-        
-    def _to_sequence(self, record):
-        return [x for x in bytearray.fromhex(str(record.value("Data")))]
     
-    def _format_before_after(self, newseq, oldseq):
-        packetname, array = self.decoder.getDiffPackets(newseq, oldseq)
-        mystring = "Packet: %s\n" % packetname
-        for key, newval, oldval in array:
-            mystring += "  {0}\n".format(key)
-            mystring += "    {0:10s}\t{1}\n".format('after:', newval)
-            mystring += "    {0:10s}\t{1}\n\n".format('before:', oldval)
-        return mystring
-        
-    def _format_raw_data(self, record):
-        data = str(record.value("Data"))
-        string = ""
-        for i in range(len(data)):
-            string += data[i]
-            if i > 0:
-                if (i+1)%60 == 0:
-                    string += "\n"
-                elif (i+1)%20 == 0:
-                    string += "\t"
-                elif (i+1)%2 == 0:
-                    string += " "
-        return string
+    def _to_packet(self, record):
+        timestamp = record.value("LastChanged")
+        packet_type = record.value("Class")
+        data = record.value("Data")
+        return XPacket(timestamp, packet_type, data)
---
+++ XSeriesTestTool/xpacket.py
@@ -0,0 +1,46 @@
+
+class XPacket:
+	def __init__(self, time_generated, packet_type, data):
+		self._time = time_generated
+		self._packet_type = packet_type
+		self._data = data
+	
+	def get_time(self):
+		return self._time
+	
+	def get_type(self):
+		return self._packet_type
+	
+	def get_data(self):
+		return [x for x in bytearray.fromhex(self._data)]
+	
+	def pretty_print(self, decoder):
+		packetname, array = decoder.getDecodedData(self.get_data())
+		mystring = "Packet: %s\n" % packetname
+		for key, value in array:
+			mystring += "    {0:50s}\t{1}\n".format(key, value)
+		return mystring
+	
+	def format_raw_data(self):
+		string = ""
+		for i in range(len(self._data)):
+			string += self._data[i]
+			if i > 0:
+				if (i+1)%60 == 0:
+					string += "\n"
+				elif (i+1)%20 == 0:
+					string += "\t"
+				elif (i+1)%2 == 0:
+					string += " "
+		return string
+	
+	def changes_since(self, old_packet, decoder):
+		newseq = self.get_data()
+		oldseq = old_packet.get_data()
+		packetname, array = decoder.getDiffPackets(newseq, oldseq)
+		mystring = "Packet: %s\n" % packetname
+		for key, newval, oldval in array:
+			mystring += "  {0}\n".format(key)
+			mystring += "    {0:10s}\t{1}\n".format('after:', newval)
+			mystring += "    {0:10s}\t{1}\n\n".format('before:', oldval)
+		return mystring
